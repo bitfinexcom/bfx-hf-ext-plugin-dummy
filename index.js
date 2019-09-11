9309a4427203d1b234ae7eff3ba8fa3205f1ae36
Cris Mihalache <me@f3rno.com>
2019-09-11 17:16:36 +0700

    (initial commit)

---
+++ index.js
@@ -0,0 +1,28 @@
+const syncTradesRange = require('./lib/models/trade/sync_range')
+const syncCandlesRange = require('./lib/models/candle/sync_range')
+const auditCandleGaps = require('./lib/models/candle/audit_gaps')
+
+module.exports = {
+  schema: {
+    Trade: {
+      schemaExchangeData: {
+        id: Number,
+      },
+
+      methods: {
+        syncRange: syncTradesRange,
+      },
+    },
+
+    Candle: {
+      schemaExchangeData: {
+        type: String,
+      },
+
+      methods: {
+        auditGaps: auditCandleGaps,
+        syncRange: syncCandlesRange,
+      }
+    }
+  },
+}
